c2d9dbff65b37fb4b835978c70010864d896ab2f
romale007 <romale0819@gmail.com>
2020-11-22 22:42:51 +0300

    1st build

---
+++ precache-manifest.f2bfaeb78ea11cd5363dbfb8b9828d59.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef0f97905d7eb2bdf613",
+    "url": "/css/app.9d2c12ef.css"
+  },
+  {
+    "revision": "a01273ec9a6e7f60d31ddd8b7aa192cb",
+    "url": "/fonts/KyivTypeBlack3.a01273ec.otf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/fonts/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/fonts/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "95363d5714d1ccb15b1341b6bd19c079",
+    "url": "/img/architecture-1171462.png"
+  },
+  {
+    "revision": "ed2b6d7456e1bc0e56b4dc59d79ca9b1",
+    "url": "/img/as.png"
+  },
+  {
+    "revision": "5bfd5c1d1f61cfb1d189e6897a754734",
+    "url": "/img/chairs-2181968.png"
+  },
+  {
+    "revision": "69fd29cf1ac15a6614d0c58ba33f284c",
+    "url": "/img/living-room.jpg"
+  },
+  {
+    "revision": "39dec5da56e478fb70d16c8fe7c37523",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef0f97905d7eb2bdf613",
+    "url": "/js/app.f79d283f.js"
+  },
+  {
+    "revision": "9ae8ba6289d5c8737c8c",
+    "url": "/js/chunk-vendors.fcd2bebd.js"
+  },
+  {
+    "revision": "0405dade9a56c93b9c135211361ff7d7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
